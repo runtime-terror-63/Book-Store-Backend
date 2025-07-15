3ee8b6463659b9972b91a52d433fd6ac0a22e123
runtime-terror-63 <sayandutta84314@gmail.com>
2025-07-15 21:24:26 +0530

    user register bugs solved🔥

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,29 +1,31 @@
 const User = require('../models/User');
 
-exports.register = async(req, res, next) =>{
-  try{
-    const {firstname, lastname, email, password} = req.body;
-    const exists = await User.findOne({email});
-    if(exists) return res.status(400).json({message: 'User already exists'});
+exports.register = async (req, res, next) => {
+  try {
+    const { firstname, lastname, email, password } = req.body;
+    const exists = await User.findOne({ email });
+    if (exists) return res.status(400).json({ message: 'User already exists' });
 
-    const user = await new User({firstname, lastname, email, password});
+    const user = new User({ firstname, lastname, email, password });
     await user.save();
 
-    req.login(user, err=>{
-      if(err) return next(err);
+    // Only send response after login
+    req.login(user, err => {
+      if (err) return next(err);
       res.status(201).json({
-        message:'Registered and logged in',
-        user:{
-          firstname:user.firstname,
-          lastname:user.lastname,
-          email:user.email
-        }
+        message: 'Registered and logged in',
+        user: {
+          firstname: user.firstname,
+          lastname: user.lastname,
+          email: user.email,
+        },
       });
     });
-  }catch(err){
+  } catch (err) {
     next(err);
   }
 };
+
 
 exports.logout = (req, res)=>{
   req.logout(()=>{
