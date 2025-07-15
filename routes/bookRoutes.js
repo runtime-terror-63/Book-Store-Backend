9c26eb9ae74f886160ab809c576adaecc6f972f0
runtime-terror-63 <sayandutta84314@gmail.com>
2025-07-15 22:38:13 +0530

    admin middleware update

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const {createBook, updateBook, deleteBook, getBooks} = require('../controllers/bookController');
+const { isAdmin } = require("../middlewares/authorize");
 const router = express.Router();
 
 function ensureAuth(req, res, next) {
   if (req.isAuthenticated()) return next();
-  res.status(401).json({ message: 'Unauthorized' });
+  res.status(401).json({ message: "Unauthorized" });
 }
 
 router.use(ensureAuth);
 
-
-router.get('/', getBooks);
-router.post('/create', createBook);
-router.put('/:id', updateBook);
-router.delete('/:id', deleteBook);
+router.get("/", getBooks);
+router.post("/create", isAdmin, createBook);
+router.put("/:id", isAdmin, updateBook);
+router.delete("/:id", isAdmin, deleteBook);
 
 
 module.exports = router;
