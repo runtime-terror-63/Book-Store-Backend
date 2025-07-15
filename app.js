e64d503dbd6535e748447c6188c84b9312ad297e
Sayan <sayandutta84314@gamil.com>
2025-07-15 11:50:08 +0530

    testing server🚀

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=>{
+  res.end('Hey Welcome to our Book Store!');
+})
+
+app.listen(3000, ()=>{
+  console.log("Server started on port 3000");
+})
