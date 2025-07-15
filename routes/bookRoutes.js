a663ab733f56facc17da5c28194ca437e15f8760
runtime-terror-63 <sayandutta84314@gmail.com>
2025-07-15 21:25:52 +0530

    authorized  books

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -2,6 +2,14 @@ const express = require('express');
 const {createBook, updateBook, deleteBook, getBooks} = require('../controllers/bookController');
 const router = express.Router();
 
+function ensureAuth(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.status(401).json({ message: 'Unauthorized' });
+}
+
+router.use(ensureAuth);
+
+
 router.get('/', getBooks);
 router.post('/create', createBook);
 router.put('/:id', updateBook);
