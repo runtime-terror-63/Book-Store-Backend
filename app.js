2c6b68082d6ab92d54f3cd171ffd760f98c7afca
runtime-terror-63 <sayandutta84314@gmail.com>
2025-07-15 21:22:51 +0530

    server proper created

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
 const express = require('express');
 const mongoose = require("./db");
+const passport = require("passport");
+require('./passportConfig')
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 require("dotenv").config();
-const bodyParser = require('body-parser');
-const authRoutes = require('./routes/authRoutes');
-const bookRoutes = require('./routes/bookRoutes');
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const bookRoutes = require("./routes/bookRoutes");
+
 const app = express();
 
+// Middleware
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=>{
-  res.end('Hey Welcome to our Book Store!');
-})
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URL,
+    }),
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hey Welcome to our Book Store!");
+});
 
 app.use(authRoutes);
-app.use('/books', bookRoutes);
+app.use("/books", bookRoutes);
 
-app.listen(3000, ()=>{
+// Start server
+app.listen(3000, () => {
   console.log("Server started on port 3000");
-})
+});
